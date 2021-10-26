50e54e1711e436d111ed550ce31ec7dc2a404143
TheHammer123 <niklas_hammer@gmx.de>
2021-10-26 13:19:16 +0200

    New Activity created (SearchStockActivity)

---
+++ app/src/main/java/com/example/stock_app/SearchStockActivity.java
@@ -0,0 +1,14 @@
+package com.example.stock_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_stock);
+    }
+}
